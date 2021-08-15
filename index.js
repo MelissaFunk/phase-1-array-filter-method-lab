3e5c1511d2e2b140952742a69742c4b6fe765fc8
+ <+>
2021-08-15 13:47:19 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(myArray, name) {
+    return myArray.filter(string => 
+        string.toLowerCase() === name.toLowerCase()
+        )
+}
+
+function fuzzyMatch(myArray, name) {
+    return myArray.filter(string =>
+        string.indexOf(name) === 0
+        )
+}
+
+function matchName(myArray, objects) {
+    return myArray.filter(string =>
+        string.name === objects
+    )
+}
